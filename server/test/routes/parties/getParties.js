12a83eba9241b2ce98f44cfdfe51eea8b10aa6d5
Malik Godwin Onimisi <despeauxz@gmail.com>
2019-01-30 01:51:33 +0100

    update test

--- server/test/routes/parties/getParties.js
+++ server/test/routes/parties/getParties.js
@@ -2,6 +2,9 @@ import request from 'supertest';
 import { expect } from 'chai';
 import app from '../../../src/app';
 
+const validID = 'fb097bde-5959-45ff-8e21-51184fa60c25';
+const invalidID = 'fb097bde-5959-45ff-8e21-51184fa60c';
+
 describe('Parties route:', () => {
     it('should get all parties', (done) => {
         request(app)
@@ -9,6 +12,30 @@ describe('Parties route:', () => {
             .end((err, res) => {
                 expect(res.statusCode).to.equal(200);
                 expect(res.body.data).to.be.a('array');
+
+            done(err);
+            });
+    });
+
+    it('should get parties by a valid ID', (done) => {
+        request(app)
+            .get(`/api/v1/parties/${validID}`)
+            .end((err, res) => {
+                expect(res.statusCode).to.equal(200);
+                expect(res.body).to.be.a('object');
+                expect(res.body).to.include.keys('data');
+
+            done(err);
+            });
+    });
+
+    it('should return errors for invalid ID', (done) => {
+        request(app)
+            .get(`/api/v1/parties/${invalidID}`)
+            .end((err, res) => {
+                expect(res.statusCode).to.equal(404);
+                expect(res.body).to.include.keys('error');
+                expect(res.body.error).to.equal('party not found');
 
             done(err);
             });
