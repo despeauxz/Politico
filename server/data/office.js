9fad424c3d67e931db0ac6cd452048f531bae0cf
Malik Godwin Onimisi <despeauxz@gmail.com>
2019-02-01 00:19:14 +0100

    test(get-office-by-ID) update test

--- server/data/office.js
+++ server/data/office.js
@@ -1,13 +1,13 @@
 export default [{
         id: '303e1797-3c0b-4dc4-8ad7-e926a165f67c',
-        typ: 'federal',
+        type: 'Federal',
         name: 'President',
         createdAt: '2019-01-31T14:34:90.000',
         updatedAt: '2019-01-31T14:34:90.000',
     },
     {
         id: 'dbf903aa-0ffe-4886-ada6-b427b823c54b',
-        type: 'local',
+        type: 'Local',
         name: 'Chairman',
         createdAt: '2019-01-31T14:34:90.000',
         updatedAt: '2019-01-31T14:34:90.000',
--- server/test/routes/offices/fetchOffice.js
+++ server/test/routes/offices/fetchOffice.js
@@ -2,6 +2,9 @@ import request from 'supertest';
 import { expect } from 'chai';
 import app from '../../../src/app';
 
+const validID = '303e1797-3c0b-4dc4-8ad7-e926a165f67c';
+const invalidID = '303e1797-3c0b-4dc4-8ad7-e926a165f63se';
+
 
 describe('Offices route: Get Offices', () => {
     it('should get all offices', (done) => {
@@ -14,4 +17,30 @@ describe('Offices route: Get Offices', () => {
             done(err);
             });
     });
+
+    describe('Get a specific Office:', () => {
+        it('should get an Office by a valid ID', (done) => {
+            request(app)
+                .get(`/api/v1/offices/${validID}`)
+                .end((err, res) => {
+                    expect(res.statusCode).to.equal(200);
+                    expect(res.body).to.be.a('object');
+                    expect(res.body).to.include.keys('data');
+
+                done(err);
+                });
+        });
+
+        it('should return errors for invalid ID', (done) => {
+            request(app)
+                .get(`/api/v1/offices/${invalidID}`)
+                .end((err, res) => {
+                    expect(res.statusCode).to.equal(404);
+                    expect(res.body).to.include.keys('error');
+                    expect(res.body.error).to.equal('Office not found');
+
+                done(err);
+                });
+        });
+    })
 });
