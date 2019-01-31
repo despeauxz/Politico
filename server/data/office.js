2cf65e1d76d7c8f6ecfb59dd6917898555de7a5a
Malik Godwin Onimisi <despeauxz@gmail.com>
2019-01-31 22:39:13 +0100

    chore(data) created dummy data for the endpoint

---
+++ server/data/office.js
@@ -0,0 +1,15 @@
+export default [{
+        id: '303e1797-3c0b-4dc4-8ad7-e926a165f67c',
+        typ: 'federal',
+        name: 'President',
+        createdAt: '2019-01-31T14:34:90.000',
+        updatedAt: '2019-01-31T14:34:90.000',
+    },
+    {
+        id: 'dbf903aa-0ffe-4886-ada6-b427b823c54b',
+        type: 'local',
+        name: 'Chairman',
+        createdAt: '2019-01-31T14:34:90.000',
+        updatedAt: '2019-01-31T14:34:90.000',
+    },
+];
