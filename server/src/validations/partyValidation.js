a199eec87f227680de38886ee12145b42ea2b6f9
Malik Godwin Onimisi <despeauxz@gmail.com>
2019-02-02 10:25:10 +0100

    added the trim sanitizer to the input

--- server/src/validations/partyValidation.js
+++ server/src/validations/partyValidation.js
@@ -4,17 +4,23 @@ import notEmpty from '../helpers/notEmpty';
 export default {
   createParty: [
     check('name')
+      .trim()
       .exists().withMessage('Name must be specified')
       .custom(value => notEmpty(value, 'Name field cannot be left blank')),
     check('fullname')
+      .trim()
       .exists().withMessage('fullname must be specified')
       .custom(value => notEmpty(value, 'Fullname field cannot be left blank')),
     check('hqAddress')
+      .trim()
       .exists().withMessage('HQ Address must be specified')
       .custom(value => notEmpty(value, 'HQ Address field cannot be left blank')),
     check('logoUrl')
+      .trim()
       .exists().withMessage('Logo URL must be specified')
-      .custom(value => notEmpty(value, 'Logo URL field cannot be left blank')),
+      .custom(value => notEmpty(value, 'Logo URL field cannot be left blank'))
+      .isURL()
+      .withMessage('Logo URL must be a URL'),
   ],
   update: [
     check('name')
