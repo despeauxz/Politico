ee8c1b8a07baff8b8fe238e26347f59c5fb3cfe9
Malik Godwin Onimisi <despeauxz@gmail.com>
2019-02-06 21:28:50 +0100

    Chore(validation) created the login validation

--- server/src/validations/userValidation.js
+++ server/src/validations/userValidation.js
@@ -2,28 +2,33 @@ import { check } from 'express-validator/check';
 import notEmpty from '../helpers/notEmpty';
 
 export default {
-  createUser: [
+  signup: [
     check('firstname')
       .trim()
-      .exists().withMessage('Firstname must be specific')
+      .exists()
+      .withMessage('Firstname must be specific')
       .custom(value => notEmpty(value, 'Firstname field cannot be left blank')),
     check('lastname')
       .trim()
-      .exists().withMessage('Lastname must be specific')
+      .exists()
+      .withMessage('Lastname must be specific')
       .custom(value => notEmpty(value, 'Lastname field cannot be left blank')),
     check('othername')
       .trim()
-      .exists().withMessage('Othername must be specific')
+      .exists()
+      .withMessage('Othername must be specific')
       .custom(value => notEmpty(value, 'Othername field cannot be left blank')),
     check('email')
       .trim()
-      .exists().withMessage('Email must be specific')
+      .exists()
+      .withMessage('Email must be specific')
       .custom(value => notEmpty(value, 'email field cannot be left blank'))
       .isEmail()
-      .withMessage('Please input an email address'),
+      .withMessage('Please input a valid email address'),
     check('digit')
       .trim()
-      .exists().withMessage('Phone number must be specified')
+      .exists()
+      .withMessage('Phone number must be specified')
       .custom(value => notEmpty(value, 'Phone number field cannot be left blank'))
       .isNumeric()
       .withMessage('Please input valid number'),
@@ -36,5 +41,19 @@ export default {
       .trim()
       .custom(value => notEmpty(value, 'Password Confirm field field cannot be left blank'))
       .custom((value, { req }) => value === req.body.password),
+  ],
+  login: [
+    check('email')
+      .trim()
+      .normalizeEmail()
+      .exists()
+      .withMessage('Email must be specific')
+      .custom(value => notEmpty(value, 'email field cannot be left blank'))
+      .isEmail()
+      .withMessage('Please input a valid email address'),
+    check('password')
+      .trim()
+      .exists().withMessage('Password must be specified')
+      .custom(value => notEmpty(value, 'Password field cannot be left blank'))
   ],
 };
