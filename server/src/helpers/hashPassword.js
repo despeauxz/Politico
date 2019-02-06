e0fa1dcff441f7688a93a06580678ad5fb29af5e
Malik Godwin Onimisi <despeauxz@gmail.com>
2019-02-06 21:36:30 +0100

    doc(helper) documented my helper function

--- server/src/helpers/hashPassword.js
+++ server/src/helpers/hashPassword.js
@@ -1,5 +1,12 @@
 import bcrypt from 'bcrypt';
 
+/**
+ * @function hashPassword
+ * @memberof UserController
+ * @param {string} password
+ * @param {integer} salt
+ * @returns
+ */
 function hashPassword(password, salt) {
   const hash = bcrypt.hashSync(password, salt);
   return hash;
