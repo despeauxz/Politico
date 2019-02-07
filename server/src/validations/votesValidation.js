10cad3fbf61c5008e62b92cae2f2110c1a2da51d
Malik Godwin Onimisi <despeauxz@gmail.com>
2019-02-07 20:26:00 +0100

    Chore(validation) added validation to votes endpoints

--- server/src/validations/votesValidation.js
+++ server/src/validations/votesValidation.js
@@ -6,13 +6,23 @@ export default {
     check('officeId')
       .exists()
       .withMessage('Office ID must be specific')
-      .custom(value => notEmpty(value, 'Firstname field cannot be left blank'))
+      .custom(value => notEmpty(value, 'Office ID field cannot be left blank'))
       .isNumeric()
       .withMessage('Office ID must be an Integer'),
     check('partyId')
       .exists()
       .withMessage('Party Id must be specific')
-      .custom(value => notEmpty(value, 'Firstname field cannot be left blank'))
+      .custom(value => notEmpty(value, 'Party ID field cannot be left blank'))
+      .isNumeric()
+      .withMessage('Office ID must be an Integer'),
+  ],
+  votes: [
+    check('officeId')
+      .custom(value => notEmpty(value, 'Office ID field cannot be left blank'))
+      .isNumeric()
+      .withMessage('Office ID must be an Integer'),
+    check('candidateId')
+      .custom(value => notEmpty(value, 'Candidate ID field cannot be left blank'))
       .isNumeric()
       .withMessage('Office ID must be an Integer'),
   ],
