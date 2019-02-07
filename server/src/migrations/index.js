3564607de0c6cb38af4c00430a1b348f0b0fa6d9
Malik Godwin Onimisi <despeauxz@gmail.com>
2019-02-07 20:22:43 +0100

    Chore(migration) updated the votes table migrations

--- server/src/migrations/index.js
+++ server/src/migrations/index.js
@@ -5,10 +5,11 @@ import configAll from '../../config/database_config';
 config();
 const env = process.env.NODE_ENV;
 const connect = configAll[env];
+// eslint-disable-next-line prefer-destructuring
 const connectionString = connect.connectionString;
 
 const pool = new Pool({
-    connectionString,
+  connectionString,
 });
 
 export default pool;
