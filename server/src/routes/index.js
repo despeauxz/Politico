b296e05d432ff40bb63420df94468cf285f8fb27
Malik Godwin Onimisi <despeauxz@gmail.com>
2019-02-07 20:23:45 +0100

    registered the votes route

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import partyRoutes from './partyRoutes';
 import officeRoutes from './officeRoutes';
+import votesRoutes from './votes';
 import userRoutes from './users';
 
 const apiRoutes = express.Router();
@@ -22,6 +23,7 @@ apiRoutes.get('/v1', (req, res) => {
 apiRoutes.use('/v1/parties', partyRoutes);
 apiRoutes.use('/v1/offices', officeRoutes);
 apiRoutes.use('/v1/auth', userRoutes);
+apiRoutes.use('/v1/', votesRoutes);
 
 
 export default apiRoutes;
