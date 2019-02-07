ec0cfb80204f641de85a8cdaa699cd61817b8854
Malik Godwin Onimisi <despeauxz@gmail.com>
2019-02-07 16:16:36 +0100

    Chore(auth) Lockdown the endpoint to assign roles

--- server/src/routes/officeRoutes.js
+++ server/src/routes/officeRoutes.js
@@ -1,15 +1,19 @@
 import express from 'express';
 import OfficeController from '../controllers/OfficeController';
 import officeValidation from '../validations/officeValidation';
+import votesValidation from '../validations/votesValidation';
 import ValidationHandler from '../middlewares/ValidationHandler';
 import Trim from '../middlewares/Trim';
+import Authorization from '../middlewares/Authorization';
 
 
 const officeRoutes = express.Router();
 const validation = [ValidationHandler.validate, Trim.trim, ValidationHandler.isEmptyReq];
+officeRoutes.use(Authorization.authenticate);
 
-officeRoutes.post('/', officeValidation.createOffice, validation, OfficeController.create);
+officeRoutes.post('/', officeValidation.createOffice, validation, Authorization.isAdmin, OfficeController.create);
 officeRoutes.get('/', OfficeController.getOffices);
 officeRoutes.get('/:id', OfficeController.getOfficeByID);
+officeRoutes.post('/:id/register', votesValidation.candidates, validation, OfficeController.candidates);
 
 export default officeRoutes;
