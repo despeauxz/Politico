0cb0885fca48a27d38aaf3b136a8b1016c0b1021
Malik Godwin Onimisi <despeauxz@gmail.com>
2019-02-18 15:50:54 +0100

    validations for the petition endpoint

---
+++ server/src/validations/petitionValidation.js
@@ -0,0 +1,21 @@
+import { check } from 'express-validator/check';
+import notEmpty from '../helpers/notEmpty';
+
+export default {
+  create: [
+    check('title')
+      .trim()
+      .exists().withMessage('Title must be specified')
+      .custom(value => notEmpty(value, 'Title cannot be left blank')),
+    check('text')
+      .trim()
+      .exists().withMessage('Text body must be specified')
+      .custom(value => notEmpty(value, 'Text body cannot be left blank')),
+    check('office_id')
+      .trim()
+      .exists().withMessage('Office Id must be specified')
+      .custom(value => notEmpty(value, 'Office Id cannot be left blank'))
+      .isInt()
+      .withMessage('Must be an Integer'),
+  ],
+};
