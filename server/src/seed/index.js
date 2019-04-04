6d369461d1020f66083423f25c84a895e6b71365
Malik Godwin Onimisi <despeauxz@gmail.com>
2019-04-04 16:06:57 +0100

    modified the seeded admin user

--- server/src/seed/index.js
+++ server/src/seed/index.js
@@ -3,5 +3,5 @@ import db from '../models';
 import hashPassword from '../helpers/hashPassword';
 
 const text = 'INSERT INTO users (firstname, lastname, othername, email, digit, is_admin, password, created_at) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)';
-const values = ['John', 'Doe', 'Berry', 'admin@email.com', '0810877621', true, hashPassword('password', 10), moment(new Date())];
+const values = ['Godwin', 'Malik', 'Onimisi', 'malikberw@gmail.com', '0810877621', true, hashPassword('password', 10), moment(new Date())];
 db.query(text, values);
